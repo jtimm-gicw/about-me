333ff0263f85b70003405ce521bff4ba8e6624bd
Jason <jtimm@gicw.org>
2024-10-31 15:26:09 -0700

    added for loops and while loops to js

--- app.js
+++ app.js
@@ -55,6 +55,76 @@ if (appear == 'yes') {
 else if (appear !== 'yes') {
     alert('You just haven\'t looked hard enough! Try again.')
 }
+*/
+
+
+// Guess the number
+let guessANumber = 'What number am I thinking of between one and 20';
+let attempts = 4;
+let answer = 16;
+let response = null;
+
+while (response !== answer) {
+
+    if (!attempts) {
+      alert('No more attempt. Nice try!');
+      break;
+    }
+
+    response = parseInt(prompt(guessANumber));
+  attempts -= 1;
+  if (response === answer) {
+    alert('You must be psychic!');
+    userscore += 1;
+  }
+
+  if (response < answer) {
+    alert('Wow, that\'s too low! Try again.');
+  }
+
+  if (response > answer) {
+    alert('Oooo, too high! Try again.');
+  }
+};
+// Guessing Game
+let userPoints = 0;
+
+console.log('Welcome to Jason\'s page!');
+
+// Guessing Game
+alert('Welcome to my guessing game!');
+
+let user = prompt('What is your name?');
+// Ensure the user provides a name (not null or empty)
+while (!user) {
+    user = prompt('Come on! Just tell me your name');
+}
+let firstName = user;
+
+// Movie list
+let favMovie = ['Indiana Jones & the Temple of Doom', 'Matrix', 'Star Wars', 'Snatch', 'Happy Gilmore'];
+let correctAnswer = 'Star Wars';
+
+alert('Try to guess Jason\'s favorite movie from the following list: ' + favMovie.join(', ') + '. You have 5 tries!');
+
+let guessedCorrectly = false; // Track if the correct movie is guessed
+
+for (let i = 0; i < 5; i++) {
+    let answer = prompt('Guess the favorite movie!').trim();
+    
+    if (answer === correctAnswer) {
+        userPoints++;
+        alert('Correct! Jason\'s favorite movie is indeed ' + correctAnswer + '.');
+        guessedCorrectly = true; // Set flag to true to indicate correct guess
+        break; // Exit the loop if guessed correctly
+    } else {
+        alert('Incorrect guess. Try again!');
+    }
+}
 
 // Final alert
-alert('Okay.' + user + '.' + 'Let\'s learn more about Jason.')
+if (!guessedCorrectly) {
+    alert('You\'ve used all your guesses! The correct answer was ' + correctAnswer + '.');
+}
+
+alert('Greetings ' + firstName + '! You have ' + userPoints + ' point(s). Thanks for playing!');
