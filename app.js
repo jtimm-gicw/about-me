af407b502fabb0b77f0495d55952ed723dd23d06
Jason <jtimm@gicw.org>
2026-01-19 12:12:38 -0800

    added profile pic and upddated styles

--- app.js
+++ app.js
@@ -1,61 +1,61 @@
 `use strict`
 //Welcome user
-/*let user = prompt('What is your name?');
+// let user = prompt('What is your name?');
 
-alert ('Hi, ' + user + '.' + ' Welcome to my site!');
+// alert ('Hi, ' + user + '.' + ' Welcome to my site!');
 
-// 1
-let person = prompt('Are you a night owl or an early bird?');
-if (person == 'early bird') {
-    alert('The early bird catches the worm!')
-}
-else if (person == 'night owl') {
-    alert('Night owls are awake until the break of dawn!')
-};
+// // 1
+// let person = prompt('Are you a night owl or an early bird?');
+// if (person == 'early bird') {
+//     alert('The early bird catches the worm!')
+// }
+// else if (person == 'night owl') {
+//     alert('Night owls are awake until the break of dawn!')
+// };
 
-// 2
-let like= prompt('Does Jason like sweets?');
-if (like == 'yes') {
-    alert('Correct! Jason has a sweet tooth.')
-}
-else if (like == 'no') {
-    alert('Sorry! His dentist would disagree with you.')
-};
+// // 2
+// let like= prompt('Does Jason like sweets?');
+// if (like == 'yes') {
+//     alert('Correct! Jason has a sweet tooth.')
+// }
+// else if (like == 'no') {
+//     alert('Sorry! His dentist would disagree with you.')
+// };
 
-// 3
-let color= prompt('What is Jason\'s favorite color? Blue or red?');
+// // 3
+// let color= prompt('What is Jason\'s favorite color? Blue or red?');
 
-if  (color == 'blue') {
-    alert('Gray-blue, actually. But, just blue is fine.')
-}
-else if (color == 'red') {
-    alert('No Jason does not like red.')
-}
-else if (color || 'blue', 'red') {
-    alert('You don\'t know Jason\'s favorite color. I\'m shocked!')
-};
+// if  (color == 'blue') {
+//     alert('Gray-blue, actually. But, just blue is fine.')
+// }
+// else if (color == 'red') {
+//     alert('No Jason does not like red.')
+// }
+// else if (color || 'blue', 'red') {
+//     alert('You don\'t know Jason\'s favorite color. I\'m shocked!')
+// };
 
-// 4
-let movie= prompt('Which movie does Jason like more...Barbie or Star Wars?');
-if (movie == 'Star Wars') {
-    alert('It\'s true. Jason has seen every Star Wars movie at least 10 times.')
-}
-else if (movie == 'Barbie') {
-    alert('I mean who doesn\'t like Barbie?  Jason, that\'s who!')
-}
-else if (movie || 'Barbie', 'Star Wars'){
-    alert('I guess you don\'t know Jason. ;)') 
-};
+// // 4
+// let movie= prompt('Which movie does Jason like more...Barbie or Star Wars?');
+// if (movie == 'Star Wars') {
+//     alert('It\'s true. Jason has seen every Star Wars movie at least 10 times.')
+// }
+// else if (movie == 'Barbie') {
+//     alert('I mean who doesn\'t like Barbie?  Jason, that\'s who!')
+// }
+// else if (movie || 'Barbie', 'Star Wars'){
+//     alert('I guess you don\'t know Jason. ;)') 
+// };
 
-// 5
-let appear= prompt('Could Brad Pitt and Jason be twins?');
-if (appear == 'yes') {
-    alert('Of course if you squint hard enogh, they could be.')
-}
-else if (appear !== 'yes') {
-    alert('You just haven\'t looked hard enough! Try again.')
-}
-*/
+// // 5
+// let appear= prompt('Could Brad Pitt and Jason be twins?');
+// if (appear == 'yes') {
+//     alert('Of course if you squint hard enogh, they could be.')
+// }
+// else if (appear !== 'yes') {
+//     alert('You just haven\'t looked hard enough! Try again.')
+// }
+
 
 
 // Guess the number
